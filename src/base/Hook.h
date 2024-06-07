b00564cd344497a6e38c7e3f80e02616fb47bae7
bakapear <proxybomb@gmail.com>
2024-06-07 21:44:22 +0200

    rename typenames

--- src/base/Hook.h
+++ src/base/Hook.h
@@ -2,30 +2,30 @@
 
 #include <subhook.h>
 
-template <typename T, typename Ret, typename... Args>
+template <typename Instance, typename Func, typename... Args>
 class MemberPointer {
  public:
-  static Ret Forwarder(Args... args) { return (_instance->*_func)(args...); }
-  static void SetFunction(Ret (T::*func)(Args...)) { _func = func; }
-  static void SetInstance(T* instance) { _instance = instance; }
+  static Func Forwarder(Args... args) { return (_instance->*_func)(args...); }
+  static void SetFunction(Func (Instance::*func)(Args...)) { _func = func; }
+  static void SetInstance(Instance* instance) { _instance = instance; }
 
  private:
-  static Ret (T::*_func)(Args...);
-  static T* _instance;
+  static Func (Instance::*_func)(Args...);
+  static Instance* _instance;
 };
 
-template <typename T, typename Ret, typename... Args>
-Ret (T::*MemberPointer<T, Ret, Args...>::_func)(Args...) = nullptr;
+template <typename Instance, typename Func, typename... Args>
+Func (Instance::*MemberPointer<Instance, Func, Args...>::_func)(Args...) = nullptr;
 
-template <typename T, typename Ret, typename... Args>
-T* MemberPointer<T, Ret, Args...>::_instance = nullptr;
+template <typename Instance, typename Func, typename... Args>
+Instance* MemberPointer<Instance, Func, Args...>::_instance = nullptr;
 
 class Hook : public subhook::Hook {
  public:
-  template <typename T, typename Ret, typename... Args>
-  bool InstallMember(void* src, Ret (T::*func)(Args...), T* instance, subhook::HookFlags flags = subhook::HookNoFlags) {
-    MemberPointer<T, Ret, Args...>::SetFunction(func);
-    MemberPointer<T, Ret, Args...>::SetInstance(instance);
-    return Install(src, (void*)&MemberPointer<T, Ret, Args...>::Forwarder, flags);
+  template <typename Instance, typename Func, typename... Args>
+  bool InstallMember(void* src, Func (Instance::*func)(Args...), Instance* instance, subhook::HookFlags flags = subhook::HookNoFlags) {
+    MemberPointer<Instance, Func, Args...>::SetFunction(func);
+    MemberPointer<Instance, Func, Args...>::SetInstance(instance);
+    return Install(src, (void*)&MemberPointer<Instance, Func, Args...>::Forwarder, flags);
   }
 };
